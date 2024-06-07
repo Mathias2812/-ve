b7ad5f78eed2788274ec5be75c8c576c44993108
Mathias2812 <166415278+Mathias2812@users.noreply.github.com>
2024-06-07 17:38:13 +0200

    Add files via upload
    ny script

--- scripts.js
+++ scripts.js
@@ -55,7 +55,7 @@ FruitArray.pop();
 console.log(FruitArray)
 
 
-// Construction Funktioner
+// Constructer Funktioner
 
 function MadFordeler(navn, farve, type, rating, smag){
     this.navn = navn;
@@ -78,13 +78,13 @@ function MadFordeler(navn, farve, type, rating, smag){
     }
 }
 
-const Mad1 = new MadFordeler("Blåbær","blå","bær",7, "sødt");
-const Mad2 = new MadFordeler("Københavnerstang", "gul", "is",10, "sødt")
+const Mad1 = new MadFordeler("Blåbær","blå","bær", 7, "sødt");
+const Mad2 = new MadFordeler("Københavnerstang", "gul", "is", 10, "sødt")
 
 console.log(Mad1);
 console.log(Mad2);
 
-//Object Literal
+//Object Literal 
 
 const fodbold = {
     farve: "Hvid",
@@ -93,4 +93,75 @@ const fodbold = {
 }
 console.log(fodbold);
 
-//
+//Object Literal Metadata
+
+let persons = ["Henrik", "Morten","Torben", "Ole" ]
+let sport = ["Fodbold", "Basketbold", "Tennis", "Gymnastik"]
+
+const meta = {
+    NumberofProps: 2,
+    props: [
+    {name:"persons", type:"Array of strings" },
+    {name:"sport", type:"Array of strings"}
+    ]
+}
+console.log(meta);
+
+
+// Sortering - Omvendt rækkefølge
+
+let names = ["Hans", "Peter", "Henrik", "Jens", "Lars", "Ole"]
+
+function invertArray(array) {
+    let invertedArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        invertedArray.push(array[i]);
+
+    }
+    return invertedArray;
+}
+
+console.log(invertArray(names));
+
+//Sortering af Array
+
+const teacherArray = ["Thomas", "Torill", "Mads", "Heine"]
+
+teacherArray.sort();
+
+console.log(teacherArray);
+
+//Sortering efter array of objects
+
+const objectArray = {
+    objects: [
+        {name: "Thomas", age: 42},
+        {name: "Torill", age: 43},
+        {name: "Mads", age: 36},
+        {name: "Heine", age: 54}
+    ]
+}
+
+objectArray.objects.sort(function(a,b){
+return a.age - b.age
+})
+
+console.log(objectArray)
+
+// Sortering efter array of strings
+
+const stringArray = [
+    [1, 30, 100],
+    [5, 25, 74],
+    [3, 16, 36],
+    [13, 47, 85],
+    [9, 32, 65]
+];
+
+stringArray.sort(function(a,b)
+{
+   return a[2] - b[2];
+}
+);
+
+console.log(stringArray);
